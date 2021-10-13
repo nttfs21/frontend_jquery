3b97e4ad7303325fc4e4c36bc8796f7f1ffcb697
nttfs21 <divyansh1727@gmail.com>
2021-10-13 16:41:47 +0530

    Initial Commit

---
+++ js/employee.js
@@ -0,0 +1,65 @@
+//var, let, const
+//Single threaded application
+//Assignments - October 13
+//POST - XMLHttpRequest
+//PUT - XMLHttpRequest, fetch
+//DELETE - XMLHttpRequest, fetch
+var employeesList;
+/*function getEmployeeList(){
+   var xhttp = new XMLHttpRequest();
+   var obj = this;
+   xhttp.onreadystatechange = function(){
+        if(xhttp.readyState === 4 && xhttp.status === 200){
+            employeesList = JSON.parse(xhttp.response);
+            obj.displayEmployees();
+        }
+    };
+    xhttp.open('GET', 'http://localhost:3000/employees');
+    xhttp.send();
+}*/
+
+(function addEmployee(){
+    console.log("In POST request")
+    let employee = {name:"Indrasen", email:"indrasen@gmail.com", active: "Y"};
+    fetch('http://localhost:3000/employees',{
+        method: 'POST',
+        headers:{
+            'Content-type': 'application/json'
+        },
+        body:  JSON.stringify(employee)
+    }).then((res)=>console.log(res)).catch((err)=>console.log(err));
+
+})();
+
+function getEmployeeList(){
+   fetch('http://localhost:3000/employees')
+   .then((res)=>{
+       res.json()
+       .then((data)=>{
+            employeesList = data;
+            displayEmployees();
+       });
+   })
+   .catch((err)=>{
+       console.log(err)
+   });
+}
+// 1,2,3,4
+// 2,4,6,8
+//newArr = arr.map((elem)=>elem*2)
+
+function displayEmployees(){
+   let employeesDispCont = employeesList.map((employee)=>`<li>${employee.name}(${employee.active?'Y':'N'})-${employee.email}</li>`);
+   console.log(employeesDispCont);
+   let element = document.getElementById('employeeList');
+   let innerHTML = "";
+   for(let index = 0; index<employeesDispCont.length; index++){
+    innerHTML = innerHTML.concat(employeesDispCont[index]);
+   }
+   element.innerHTML = innerHTML;
+}
+
+window.onload = ()=>{
+    let element = document.getElementsByName('username')[0];
+    element.value = 'John';
+}
